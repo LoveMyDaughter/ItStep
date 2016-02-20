18aea9a12bdc399e0d7739c6b132189139cd599d
LoveMyDaughter <tsav1986@gmail.com>
2016-02-20 09:44:09 +0200

    Added files via upload

---
+++ TestClass1.java
@@ -0,0 +1,12 @@
+package ua.itstep.work;
+
+import java.util.logging.Logger;
+
+public class TestClass1 {
+	
+	Calc c = null;
+	static Logger log = Logger.getLogger(TestClass.class.getName());
+	public static void main(String [] dsa){
+		log.warning("dsadsa");
+	}
+}
